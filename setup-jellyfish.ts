acf76195011d7a1cf02bf84948abf17b8974e4b3
Konrad Bochnia <kbochnia@gmail.com>
2023-11-10 15:43:46 +0100

    Add debug console logs

--- setup-jellyfish.ts
+++ setup-jellyfish.ts
@@ -9,12 +9,16 @@ export default async function setupJellyfish() {
     .filter(({internal}) => !internal)
     .map(({address}) => address)[0];
     
+  console.log("Attempting to start container, EXTERNAL_IP: ", EXTERNAL_IP);
+    
   const container = await new DockerComposeEnvironment(".", "docker-compose-test.yaml").withEnvironment({EXTERNAL_IP})
   .withWaitStrategy(
     "jellyfish",
     Wait.forLogMessage("Access JellyfishWeb.Endpoint at")
   )
   .up();
+  
+  console.log("container should be running ", container.getContainer("jellyfish")?.getId());
   
   setupState.jellyfishContainer = container;
 }
