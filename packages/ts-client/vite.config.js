f78cf711c6821f0bcd063c1ef1ab2997a5ac51c2
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-06-20 16:51:53 +0200

    TS client POC

---
+++ packages/ts-client/vite.config.js
@@ -0,0 +1,32 @@
+import { resolve } from "path";
+
+import { defineConfig } from "vitest/config";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json";
+
+export default defineConfig({
+  build: {
+    target: "es2018",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ts-client",
+      fileName: "ts-client",
+    },
+    // rollupOptions: {
+    //   external: Object.keys(pkg.dependencies || {}),
+    //   output: {
+    //     globals: {
+    //       events: "EventEmitter",
+    //       uuid: "uuid",
+    //     },
+    //   },
+    // },
+  },
+  // test: {
+  //   exclude: [...configDefaults.exclude, "e2e"],
+  //   coverage: {
+  //     include: ["src"],
+  //   },
+  // },
+  plugins: [dts({include: "./src/**/*", rollupTypes: true})],
+});
